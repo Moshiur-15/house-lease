a941c335897e4b609afd6a72387d5f3ab9fa53d1
Moshiur-15 <masiurislam28@gmail.com>
2025-06-12 13:34:52 +0600

    add blog

--- src/app/Components/seller/GetData.js
+++ src/app/Components/seller/GetData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function GetPropertiesData() {
   try {
-    const response = await axios.get('http://localhost:3000/api/seller/property');
+    const response = await axios.get('https://house-lease-6p94zuqie-moshiur-15s-projects.vercel.app/api/seller/property');
     return response.data.property;
   } catch (error) {
     console.error('Failed to fetch properties:', error);
