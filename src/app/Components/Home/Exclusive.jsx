a38e6c481d3cc94fb6d5d4b8afb724ea55d2caa9
Moshiur-15 <masiurislam28@gmail.com>
2025-06-17 15:49:08 +0600

    disply property error

--- src/app/Components/Home/Exclusive.jsx
+++ src/app/Components/Home/Exclusive.jsx
@@ -1,13 +1,23 @@
 import { viewAllProperty } from "../Admin/viewAllProperty";
 import ExclusiveCard from "./ExclusiveCard";
+
 const Exclusive = async () => {
   const properties = await viewAllProperty();
+
+  if (properties.length === 0) {
+    return (
+      <div className="text-center py-10 text-red-500 font-semibold text-lg">
+        No featured properties found.
+      </div>
+    );
+  }
 
   return (
     <div className="px-4 sm:px-6 lg:px-24">
       <div className="flex flex-col space-y-1 text-center">
         <span className="text-xs font-semibold tracking-widest flex justify-center items-center gap-1 uppercase">
-          <div className="h-2.5 w-2.5 rounded-full bg-[#EA703B]"></div>Exclusive
+          <div className="h-2.5 w-2.5 rounded-full bg-[#EA703B]"></div>
+          Exclusive
         </span>
         <h2 className="text-3xl font-bold text-gray-900">
           Featured Properties
@@ -16,7 +26,7 @@ const Exclusive = async () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
         {properties
-          ?.slice()
+          .slice()
           .reverse()
           .slice(0, 6)
           .map((property) => (
